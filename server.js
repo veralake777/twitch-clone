aa0cd6a2207387e5110784b6290979b33e801023
VeraButler <verabutlerdesign@gmail.com>
2020-06-30 15:36:33 -0400

    test

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const path = require("path");
-const port = process.env.PORT || 8080;
+const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(port);
-console.log('Server started');
+app.listen(9000);
