4e6d16c8ec3960cc45766288ff9eb4cbe6812bd3
VeraButler <verabutlerdesign@gmail.com>
2020-06-30 13:47:25 -0400

    Updates

---
+++ precache-manifest.80e07ff3ca4d8849ed7bac723db712ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb712e27a8b9806405bb",
+    "url": "/twitch-clone/static/js/main.dff966f3.chunk.js"
+  },
+  {
+    "revision": "608a647f2656e1d83c80",
+    "url": "/twitch-clone/static/js/runtime~main.80837314.js"
+  },
+  {
+    "revision": "df01a11cfdbbb3b75496",
+    "url": "/twitch-clone/static/js/2.51f79448.chunk.js"
+  },
+  {
+    "revision": "7f54a24024a14bf77424df11fda6145a",
+    "url": "/twitch-clone/index.html"
+  }
+];
